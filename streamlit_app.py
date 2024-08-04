0fa7b43853fa356c4e267f408778cdf731a31f6f
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 04:35:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,12 +115,11 @@ if selected == 'Learn about Data':
                     legend_name=variable_option
         )
         choropleth.geojson.add_to(map)
-        st_map = st_folium(map, width=900, height=450)
-
-    
-    # choropleth.geojson.add_child(
-        #     folium.features.GeoJsonTooltip(['name', 'population', 'per_100k'], labels=False)
-        # )
+        st_map = st_folium(map, width=900, height=450) 
+        
+        choropleth.geojson.add_child(
+            folium.features.GeoJsonTooltip(['ADM2_EN'], labels=True)
+        )
 
     
     if year_option == "2023" and variable_option != None:
