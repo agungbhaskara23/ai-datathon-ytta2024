b91c4d700eab366b8b8002983f60bfcb717c9cb3
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 04:13:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 import folium
+from streamlit_folium import st_folium
 
 # Sidebar for navigation
 with st.sidebar:
