98a1ada56db07472f391f4741673b1fe9e977483
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 22:12:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,10 @@ if selected == 'About Page':
     st.write('Peningkatan akan ketahanan atau pengurangan kerentanan penyakit DBD dapat menjadi upaya mengurangi dampak infeksi virus *dengue* di semua tingkatan (Hagenlocher et al., 2013; Hanifah Septiani et al., 2021). Oleh karena itu, penting untuk mengukur tingkat kerentanan '
              'infeksi DBD di setiap wilayah sebagai upaya estimasi kewaspadaan pemerintah dan masyarakat terhadap DBD. Namun demikian, pengukuran tingkat kerentanan terhadap DBD saat ini masih jarang dilakukan, terlebih di wilayah Indonesia. Pemanfaatan kecerdasan buatan (*artificial intelegence*/AI) '
              'yang terus berkembang dapat dimanfaatkan sebagai salah satu metode inovatif dalam menyelesaikan masalah, tak terkecuali dalam upaya pencegahan dan penanggulangan DBD.')
+
+    st.title('What's Inside?')
+    st.write('Dalam dashboard ini tersedia empat menu utama, yaitu: ')
+    
 if selected == 'Analisis Deskriptif Variabel':
     # page title
     st.title('Variabel-variabel Penyusun Indeks')
