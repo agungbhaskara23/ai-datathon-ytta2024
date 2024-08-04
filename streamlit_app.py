5d17568de67f06ffd72d899d55393b8bd6856dd8
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 04:16:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,9 +101,10 @@ if selected == 'Learn about Data':
         col4.metric("Tertinggi", round(df_2022[variable_option].max(),4), round((df_2022[variable_option].max()) - (df_2021[variable_option].max()),4))
         col5.metric("Standar Deviasi", round(df_2022[variable_option].std(),4), round((df_2022[variable_option].std()) - (df_2021[variable_option].std()),4))
 
+        json1 = f"data/shp_java_kabkota.geojson"
         map = folium.Map(location=[-7.244198, 109.616631], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
         choropleth = folium.Choropleth(
-                    geo_data='data/shp_java_kabkota.geojson',
+                    geo_data=json1,
                     data=df_2022,
                     columns=('KAB/KOT', variable_option),
                     key_on='feature.properties.name',
