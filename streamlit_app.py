7a727c88b93fac4ace62afde7e751036379c20da
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:12:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,13 @@ from streamlit_option_menu import option_menu
 
 st.title('Dashboard Indeks Kerentanan Penyakit Demam Berdarah Dengue (DBD)')
 
-# Set page configuration
-st.set_page_config(page_title="Health Assistant",
-                   layout="wide",
-                   page_icon="🧑‍⚕️")
+# sidebar for navigation
+with st.sidebar:
+    selected = option_menu('Dashboard Indeks Kerentanan Penyebaran Penyakit DBD (IK-DBD)',
+                           ['About Page', 'Analisis Deskriptif Variabel', 
+                            'Indeks Kerentanan Penyakit DBD (IK-DBD)','Prediksi Nilai IK-DBD'],
+                           menu_icon='hospital',
+                           icons=['house', 'gear', 'person', 'app'],
+                           default_index=0)
+
+
