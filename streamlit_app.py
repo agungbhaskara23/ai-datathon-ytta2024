0ed777a70d6bb00f9807cdc58cfe26b1d04bf6fb
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 02:12:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from streamlit_option_menu import option_menu
 
 # Sidebar for navigation
 with st.sidebar:
-    selected = option_menu('Dashboard Indeks Kerentanan Penyebaran Penyakit DBD (IK DBD)',
+    selected = option_menu('Menu Dashboard IK DBD)',
                            ['About Page', 'Analisis Deskriptif Variabel', 
                             'Indeks Kerentanan Penyakit DBD (IK DBD)','Prediksi Nilai IK DBD'],
                            menu_icon='hospital',
@@ -14,35 +14,32 @@ with st.sidebar:
 # About Page
 if selected == 'About Page':
     # page title
-    st.title('Introduction')
+    st.title('Welcome to Dashboard IK DBD!')
     
     # background story
-    st.write('Perubahan iklim merupakan fenomena global yang semakin nyata dirasakan di berbagai belahan dunia. Menurut PBB, **perubahan iklim mengacu pada perubahan atau pergeseran jangka panjang dalam suhu dan pola cuaca**. '
-    'Perubahan iklim memiliki sejumlah dampak, seperti menurunnya kemampuan untuk menanam pangan, kesulitan pada sektor perumahan, keselamatan dan pekerjaan, hingga perubahan iklim mampu mempengaruhi kesehatan manusia (Susilawati, 2021). ' 
-    'Dalam aspek kesehatan, perubahan iklim dapat meningkatkan risiko terhadap maraknya penyebaran penyakit menular. Salah satu penyakit menular tersebut adalah penyakit demam berdarah *dengue* (DBD) yang menular melalui vektor/media tertentu.')
-
-    st.write('DBD merupakan penyakit menular yang disebabkan oleh virus *dengue* yang ditularkan melalui gigitan nyamuk *aedes aegypti* dan *aedes albopictus* (Soedarto, 2018). Pada tahun 2024, *World Health Organization* atau WHO memperkirakan setengah '
-             'dari populasi dunia berisiko terkena virus *dengue*, dengan 100 - 400 juta infeksi setiap tahun. Penyakit DBD juga masih menjadi salah satu masalah utama dari kesehatan masyarakat di Indonesia (Kusumawardani & Achmadi, 2012). Pada tahun 2010, '
-             'Indonesia pernah berada pada urutan kedua dengan jumlah kasus DBD terbanyak di Asia (Bhatt et al., 2013). Disamping itu, Kementerian Kesehatan RI (2021) melaporkan bahwa lebih dari 80% kabupaten/kota di setiap provinsinya terindikasi penyakit DBD.')
-
-    st.write('Peningkatan akan ketahanan atau pengurangan kerentanan penyakit DBD dapat menjadi upaya mengurangi dampak infeksi virus *dengue* di semua tingkatan (Hagenlocher et al., 2013; Hanifah Septiani et al., 2021). Oleh karena itu, penting untuk mengukur tingkat kerentanan '
-             'infeksi DBD di setiap wilayah sebagai upaya estimasi kewaspadaan pemerintah dan masyarakat terhadap DBD. Namun demikian, pengukuran tingkat kerentanan terhadap DBD saat ini masih jarang dilakukan, terlebih di wilayah Indonesia. Pemanfaatan kecerdasan buatan (*artificial intelegence*/AI) '
-             'yang terus berkembang dapat dimanfaatkan sebagai salah satu metode inovatif dalam menyelesaikan masalah, tak terkecuali dalam upaya pencegahan dan penanggulangan DBD.')
+    st.write('Demam berdarah *dengue* atau DBD merupakan penyakit menular yang disebabkan oleh virus *dengue* yang ditularkan melalui gigitan nyamuk *aedes aegypti* dan *aedes albopictus*. Pada tahun 2024, *World Health Organization* atau WHO memperkirakan setengah '
+             'dari populasi dunia berisiko terkena virus *dengue*, dengan 100 - 400 juta infeksi setiap tahun. Penyakit DBD juga masih menjadi salah satu masalah utama dari kesehatan masyarakat di Indonesia karena pada tahun 2010, Indonesia pernah berada pada urutan '
+             'kedua dengan jumlah kasus DBD terbanyak di Asia **(Bhatt et al., 2013)**. Disamping itu, Kementerian Kesehatan RI (2021) melaporkan bahwa lebih dari 80% kabupaten/kota di setiap provinsinya terindikasi penyakit DBD.')
+    
+    st.write('Dashboard IK DBD merupakan dashboard untuk pemetaan tingkat kerentanan penyakit demam berdarah *dengue* yang terjadi di masyarakat Indonesia. Saat ini, dashboard masih hanya berfokus pada pemetaan tingkat kerentanan di Pulau Jawa. Peningkatan akan ketahanan '
+             'atau pengurangan kerentanan penyakit DBD dapat menjadi upaya mengurangi dampak infeksi virus *dengue* di semua tingkatan *(Hagenlocher et al., 2013; Hanifah Septiani et al., 2021)*. Adanya perubahan iklim sebagai salah satu fenomena global memberikan sejumlah dampak, '
+             'seperti menurunnya kemampuan untuk menanam pangan, kesulitan pada sektor perumahan, keselamatan dan pekerjaan, hingga perubahan iklim mampu mempengaruhi kesehatan manusia (Susilawati, 2021), salah satunya adalah penyebaran penyakit menular. '
+             'Pemanfaatan kecerdasan buatan (*artificial intelegence*/AI) yang terus berkembang dapat dimanfaatkan sebagai salah satu metode inovatif dalam menyelesaikan masalah, tak terkecuali dalam upaya pencegahan dan penanggulangan DBD.')
 
     st.title("What's Inside?")
     st.write('Dalam dashboard ini tersedia empat menu utama, yaitu: ')
 
     st.write('**1. About Page**')
-    st.write('Berisi tentang informasi dashboard mencakup latar belakang, fitur yang tersedia, dan cara penggunaan dashboard')
+    st.write('Informasi dashboard mencakup latar belakang, fitur yang tersedia, dan cara penggunaan dashboard')
 
-    st.write('**2. Analisis Deskriptif Variabel**')
-    st.write('Berisi tentang informasi variabel secara deskriptif yang digunakan sebagai penyusun indeks. Informasi disajikan dalam bentuk visual')
+    st.write('**2. Learn about Data**')
+    st.write('Informasi variabel secara deskriptif yang digunakan sebagai penyusun indeks dan disajikan dalam sejumlah bentuk visual')
 
     st.write('**3. Indeks Kerentanan Penyakit DBD (IK DBD)**')
-    st.write('Berisi tentang hasil penghitungan IK DBD pada wilayah di Indonesia yang disajikan dalam bentuk visualisasi peta hasil cluster. Selain itu, tersedia juga interpretasi dari nilai IK DBD dan cluster yang terbentuk')
+    st.write('Hasil penghitungan IK DBD pada wilayah Pulau Jawa, Indonesia pada tahun tertentu (2022). Hasil disajikan dalam berbagai visualisasi, seperti grafik dan peta hasil cluster. Interpretasi dari nilai IK DBD pada masing-masing cluster juga tersaji dalam bagian ini')
 
     st.write('**4. Prediksi Nilai IK DBD**')
-    st.write('Berisi tentang simulasi hasil kluster nilai IK DBD yang dapat dilakukan dengan inputasi nilai manual pada masing-masing variabel penyusun indeks')
+    st.write('Simulasi nilai IK DBD dan kelas cluster yang diperoleh ketika dilakukan inputasi nilai seecara manual pada masing-masing variabel penyusun indeks')
 
 if selected == 'Analisis Deskriptif Variabel':
     # page title
@@ -62,9 +59,9 @@ if selected == 'Analisis Deskriptif Variabel':
         # Row A
         st.markdown('### Summary Statistics')
         col1, col2, col3 = st.columns(3)
-        col1.metric("Tertinggi", round(df_2020[variable_option].max(),4), "1.2 °F")
-        col2.metric("Terendah", round(df_2020[variable_option].min(),4), "-8%")
-        col3.metric("Rata-rata", round(df_2020[variable_option].sum()/len(df_2020),4), "4%")
+        col1.metric("Tertinggi", round(df_2023[variable_option].max(),4), "1.2 °F")
+        col2.metric("Terendah", round(df_2023[variable_option].min(),4), "-8%")
+        col3.metric("Rata-rata", round(df_2023[variable_option].sum()/len(df_2020),4), "4%")
 
     if variable_option == "NDBI":
         st.write("Haloo NDBI")
