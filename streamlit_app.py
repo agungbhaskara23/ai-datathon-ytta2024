b8a0ff3f613c111f332a09a1b16d2bc9656214b1
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:14:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 
-st.title('Dashboard Indeks Kerentanan Penyakit Demam Berdarah Dengue (DBD)')
-
 # Sidebar for navigation
 with st.sidebar:
     selected = option_menu('Dashboard Indeks Kerentanan Penyebaran Penyakit DBD (IK-DBD)',
