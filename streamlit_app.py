5ce8c4b41f57e32d3f05326f02413f45c71e5013
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 04:40:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ if selected == 'Learn about Data':
                     legend_name=variable_option
         )
         choropleth.geojson.add_to(map)
-        st_map = st_folium(map, width=700, height=450) 
+        st_map = st_folium(map, width=1200, height=450) 
         
         choropleth.geojson.add_child(
             folium.features.GeoJsonTooltip(['ADM2_EN'], labels=False)
