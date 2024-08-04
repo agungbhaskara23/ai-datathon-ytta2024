635b3af1f43988417b92ca6e890d2748e80edfda
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:00:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,45 @@
 import streamlit as st
 
+# Set page configuration
+st.set_page_config(page_title="Dashboard IK-DBD",
+                   layout="wide",
+                   page_icon="📙")
+
+# sidebar for navigation
+with st.sidebar:
+    selected = option_menu('Dashboard Indeks Kerentanan Penyebaran Penyakit DBD (IK-DBD)',
+                           ['About Page',
+                            'Analisis Deskriptif Variabel',
+                            'Indeks Kerentanan Penyakit DBD (IK-DBD)',
+                            'Prediksi Nilai IK-DBD'],
+                           menu_icon='hospital',
+                           icons=['house', 'gear', 'person', 'app'],
+                           default_index=0)
+
+
+# About Page
+if selected == 'About Page':
+
+    # page title
+    st.title('Introduction')
+
+    #
+
+# Analisis Deskriptif Variabel
+elif selected == 'Analisis Deskriptif Variabel':
+
+  # page title
+
+
+elif selected == 'Indeks Kerentanan Penyakit DBD (IK-DBD)':
+
+  # page title
+
+
+elif selected == 'Prediksi Nilai IK-DBDl':
+
+  # page title
+
 st.title('🎈 App Name')
 
 st.write('Hello world!')
