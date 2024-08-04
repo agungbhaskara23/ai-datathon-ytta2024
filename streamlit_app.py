b9ee0e3e690acf98980438f2ba602ef45b145877
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:23:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ if selected == 'Analisis Deskriptif Variabel':
     st.title('Variabel-variabel Penyusun Indeks')
     
     # import data
-    dataset = pd.read_excel('https://raw.githubusercontent.com/agungbhaskara23/ai-datathon-ytta2024/blob/master/Dataset-Olah-2024.xlsx')
+    dataset = pd.read_excel('https://github.com/agungbhaskara23/ai-datathon-ytta2024/raw/master/Dataset-Olah-2024.xlsx')
     dataset
