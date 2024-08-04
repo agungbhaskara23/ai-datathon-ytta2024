3e1c396e79e40121935b95f058a6433b27693859
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:07:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
+from streamlit_option_menu import option_menu
 
 st.title('Dashboard Indeks Kerentanan Penyakit Demam Berdarah Dengue (DBD)')
+
+# Set page configuration
+st.set_page_config(page_title="Health Assistant",
+                   layout="wide",
+                   page_icon="🧑‍⚕️")
