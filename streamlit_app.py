708ecd01e1ab4c72f6eac2c94af0c3f355b89f18
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 21:06:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,4 @@
 import streamlit as st
 import pandas as pd
 
-# # Set page configuration
-# st.set_page_config(page_title="Dashboard IK-DBD",
-#                    layout="wide",
-#                    page_icon="📙")
-
-# sidebar for navigation
-with st.sidebar:
-    selected = option_menu('Dashboard Indeks Kerentanan Penyebaran Penyakit DBD (IK-DBD)',
-                           ['About Page',
-                            'Analisis Deskriptif Variabel',
-                            'Indeks Kerentanan Penyakit DBD (IK-DBD)',
-                            'Prediksi Nilai IK-DBD'],
-                           menu_icon='hospital',
-                           icons=['house', 'gear', 'person', 'app'],
-                           default_index=0)
-
-
-# About Page
-if selected == 'About Page':
-
-    # page title
-    st.title('Introduction')
-
-    #
-
-# # Analisis Deskriptif Variabel
-# if selected == 'Analisis Deskriptif Variabel':
-
-#   # page title
-
-
-# if selected == 'Indeks Kerentanan Penyakit DBD (IK-DBD)':
-
-#   # page title
-
-
-# if selected == 'Prediksi Nilai IK-DBDl':
-
-#   # page title
+st.title('Dashboard Indeks Kerentanan Penyakit Demam Berdarah Dengue (DBD)')
