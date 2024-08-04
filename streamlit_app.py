ebf3fdff996aacd8ceeaf0da2a9541e8dc5dc973
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 02:54:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,18 +73,18 @@ if selected == 'Learn about Data':
         data_used = "df_" + year_option
         st.markdown('### Summary Statistics')
         col1, col2, col3, col4, col5 = st.columns(5)
-        col1.metric("Rata-rata", round(df_2020[variable_option].mean(),4))
-        col2.metric("Terendah", round(df_2020[variable_option].min(),4))
-        col3.metric("Median", round(df_2020[variable_option].median(),4))
-        col4.metric("Tertinggi", round(df_2020[variable_option].max(),4))
-        col5.metric("Standar Deviasi", round(df_2020[variable_option].std(),4))
+        col1.metric("Rata-rata", round(df_2020[variable_option].mean(),4), 0)
+        col2.metric("Terendah", round(df_2020[variable_option].min(),4), 0)
+        col3.metric("Median", round(df_2020[variable_option].median(),4), 0)
+        col4.metric("Tertinggi", round(df_2020[variable_option].max(),4), 0)
+        col5.metric("Standar Deviasi", round(df_2020[variable_option].std(),4), 0)
 
     if year_option == "2021" and variable_option != None:
         
         data_used = "df_" + year_option
         st.markdown('### Summary Statistics')
         col1, col2, col3, col4, col5 = st.columns(5)
-        col1.metric("Rata-rata", round(df_2021[variable_option].mean(),4), 0.5)
+        col1.metric("Rata-rata", round(df_2021[variable_option].mean(),4), round(df_2021[variable_option].mean(),4) - round(df_2020[variable_option].mean(),4))
         col2.metric("Terendah", round(df_2021[variable_option].min(),4), )
         col3.metric("Median", round(df_2021[variable_option].median(),4), )
         col4.metric("Tertinggi", round(df_2021[variable_option].max(),4), )
