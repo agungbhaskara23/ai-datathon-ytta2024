f364152dfb3e48035ff959b2e9c0fba09e2f7983
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-04 22:49:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,12 @@ if selected == 'Analisis Deskriptif Variabel':
     )
 
     if variable_option == "NDVI":
-        st.write("Haloo NDVI")
+        # Row A
+        st.markdown('### Metrics')
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Temperature", "70 °F", "1.2 °F")
+        col2.metric("Wind", "9 mph", "-8%")
+        col3.metric("Humidity", "86%", "4%")
 
     if variable_option == "NDBI":
         st.write("Haloo NDBI")
