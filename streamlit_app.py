3786b4d9f64f6c1e21ea20db48ed8c55fa51aaad
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 13:55:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -382,7 +382,7 @@ if selected == 'Prediksi Nilai IK DBD':
         weighted_sum
 
         # Calculate the index with new dataset
-        count_index = (weighted_sum * input_scaled
+        count_index = (weighted_sum * input_scaled)
         count_index
         
         # # Normalize the count_index to the range [0, 1]
