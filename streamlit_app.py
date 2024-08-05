7d6b953a7eb5650de1973dd16d82b17a18f8d411
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-05 08:49:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,10 +216,11 @@ if selected == 'Indeks Kerentanan Penyakit DBD (IK DBD)':
             '(IK DBD) dibangun dengan menggunakan dataset pada tahun 2020, 2021, dan 2023. Bobot dihitung dengan menggunakan metode PCA')
     st.write('Hasil perolehan nilai indeks kemudian dilanjutkan dengan proses *clustering* atau pengelompokkan untuk melihat pola kedekatan atau pengelompokkan antarnilai indeks.')
 
+    df_final = pd.read_csv('https://raw.githubusercontent.com/agungbhaskara23/ai-datathon-ytta2024/master/data/df_final_with_cluster.csv')
     col1, col2, col3, col4 = st.columns(4)
-    col1.metric("Tertinggi", 23, "-8")
-    col2.metric("Terendah", 23, "-8")
-    col3.metric("Rata-rata", 23, "-8")
+    col1.metric("Tertinggi", df_final['Index'].max())
+    col2.metric("Terendah", df_final['Index'].min())
+    col3.metric("Rata-rata",df_final['Index'].mean())
 
     df_final = pd.read_csv('https://raw.githubusercontent.com/agungbhaskara23/ai-datathon-ytta2024/master/data/df_final_with_cluster.csv')
     # with col4:
