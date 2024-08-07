5137c8881e46076ef40a61ee3ae7c9cf02ab24c5
agungbhaskara23 <agungbhaskara35@gmail.com>
2024-08-07 10:59:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -376,11 +376,13 @@ if selected == 'Prediksi Nilai IK DBD':
 
         scaler = StandardScaler()
         input_scaled = scaler.fit_transform(input_df)      
-        weighted_sum = pd.read_csv('https://raw.githubusercontent.com/agungbhaskara23/ai-datathon-ytta2024/master/data/weight_pca.csv')
+        weighted_sum_raw = pd.read_csv('https://raw.githubusercontent.com/agungbhaskara23/ai-datathon-ytta2024/master/data/weight_pca.csv')
+        weighted_sum = np.array(weighted_sum_raw)
+        st.write(weighted_sum)
         weighted_sum.shape
 
         st.write(type(input_df))
-        st_write(type(weighted_sum))
+        st.write(type(weighted_sum))
         
         # # Calculate the index with new dataset
         # count_index = input_scaled.dot(weighted_sum_transpose)
